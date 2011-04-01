bbb902137adeab757d22ede60de1b5f22cd6a4ae
Dhaivatpandya <dhaivat@dhaivat-VirtualBox.(none)>
2011-04-01 17:49:40 -0500

    Added some basic code, got a window up.

---
+++ src/init.h
@@ -0,0 +1,8 @@
+#include "SDL/SDL.h"
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+#define INIT_H 1
+
+void startSDL();
+SDL_Surface* getScreen();
--- src/main.c
+++ src/main.c
@@ -3,7 +3,12 @@
 
 #include "SDL/SDL.h"
 
-int main (void) {
+#include "init.h"
 
+int main (void) {
+	/* initialize variables, call utilities, functions for this held in init.c */
+	startSDL();
+	SDL_Surface *screen = getScreen();
+	/* call gameloop */
 	return 0;
 }
